fe20a9dfc8388cd1e6db4a5caeb34194f907123f
Sujal Kalra <114348922+sujalkalra@users.noreply.github.com>
2025-04-06 09:52:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -775,7 +775,7 @@ def render_content():
 
 @rt('/')
 def get():
-    return Titled(render_content())
+    return Titled("Suji's Guestbook", render_content())
 
 @rt("/submit-message", methods=["post"])
 def post(name: str, message: str):
